3e52814768315958c175177fef9316bb19891dcb
kishore-05-spec <kishorehari172@gmail.com>
2026-01-21 14:12:05 +0530

    Update second_main.py

--- services/second_main.py
+++ services/second_main.py
@@ -18,3 +18,24 @@ WHERE
         lower(regexp_replace(:val, '[^a-zA-Z0-9 ]', '', 'g'))
     ) >= 0.5
 ORDER BY weightage DESC;
+
+
+SELECT
+    r."RuleId" AS rule_id,
+    r."RuleName",
+    1 AS weightage,
+    similarity(
+        regexp_replace(lower(gp."GuideLineParagraphData"), '[^a-z0-9 ]', '', 'g'),
+        regexp_replace(lower(:val), '[^a-z0-9 ]', '', 'g')
+    ) AS match_score
+FROM "GuidelineParagraphRules" gpr
+INNER JOIN "GuidelineParagraph" gp
+    ON gpr."Paragraphid" = gp."Id"
+INNER JOIN "Rules" r
+    ON gpr."RuleId" = r."Id"
+WHERE
+    similarity(
+        regexp_replace(lower(gp."GuideLineParagraphData"), '[^a-z0-9 ]', '', 'g'),
+        regexp_replace(lower(:val), '[^a-z0-9 ]', '', 'g')
+    ) >= 0.5
+ORDER BY match_score DESC;
